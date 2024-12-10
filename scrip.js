febbbf3919ff9a97825c89284a31d0940ef1b815
djxshin <djatlienriseup@gmail.com>
2024-12-09 20:12:29 -0800

    2nd change to all

--- scrip.js
+++ scrip.js
@@ -1,9 +1,9 @@
 function toggleDetails(element) {
     const details = document.createElement('div');
-    details.textContent = "More details about " + element.textContent;
+    details.textContent = "Details about " + element.textContent;
     details.style.marginTop = '5px';
+    details.style.fontSize = '12px';
     details.style.color = '#555';
-    details.style.fontSize = '0.9em';
 
     if (element.nextElementSibling && element.nextElementSibling.tagName === 'DIV') {
         element.parentNode.removeChild(element.nextElementSibling);
