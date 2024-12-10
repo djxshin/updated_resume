1c201c393cf491d0a1a324c40918d6af6d0f80cb
djxshin <djatlienriseup@gmail.com>
2024-12-09 20:25:03 -0800

    third commit

--- scrip.js
+++ scrip.js
@@ -1,13 +1,7 @@
 function toggleDetails(element) {
-    const details = document.createElement('div');
-    details.textContent = "Details about " + element.textContent;
-    details.style.marginTop = '5px';
-    details.style.fontSize = '12px';
-    details.style.color = '#555';
-
-    if (element.nextElementSibling && element.nextElementSibling.tagName === 'DIV') {
-        element.parentNode.removeChild(element.nextElementSibling);
-    } else {
-        element.parentNode.insertBefore(details, element.nextSibling);
+    const details = element.querySelector('.details');
+    if (details) {
+        details.style.display = details.style.display === 'none' || details.style.display === '' ? 'block' : 'none';
     }
 }
+
